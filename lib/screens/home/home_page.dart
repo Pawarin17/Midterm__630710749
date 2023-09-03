5e88c83f66ef147eb84e22b25c95800be8601045
Pawarin <malee_p@silpakorn.edu>
2023-09-03 12:07:26 +0700

    https://github.com/Pawarin17/Midterm__630710749.git

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,9 +1,10 @@
 // ignore_for_file: avoid_unnecessary_containers
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 // TODO: ใส่รหัสนักศึกษาที่ค่าสตริงนี้
-const studentId = 'Student ID';
+const studentId = '630710749';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,10 +14,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var value1 = false;
+
   @override
   Widget build(BuildContext context) {
     var textTheme = Theme.of(context).textTheme;
-
     return Scaffold(
       body: SafeArea(
         child: Container(
@@ -40,25 +42,64 @@ class _HomePageState extends State<HomePage> {
                   textAlign: TextAlign.center,
                   style: textTheme.headlineSmall!.copyWith(
                       fontWeight: FontWeight.bold, color: Colors.black87)),
-              Spacer(),
-              _buildQuizView(),
-              Spacer(),
-              _buildButtonPanel(),
               SizedBox(height: 16.0),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Question 1 to 30',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Center(
+                  child: GestureDetector(
+                    onTap: () => {},
+                    child: Container(
+                      padding: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 3.0, color: Colors.black),
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      child: Text(
+                        'What is the capital of France',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Center(
+                  child: GestureDetector(
+                    onTap: () => {},
+                    child: Container(
+                      padding: EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 2.0, color: Colors.blue),
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: Text(
+                        'A  Paris',
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
+
         ),
       ),
     );
-  }
-
-  _buildQuizView() {
-    // TODO: build UI
-    return Center(child: Text('TODO: build UI'));
-  }
-
-  _buildButtonPanel() {
-    // TODO: build UI
-    return Center(child: Text('TODO: build UI'));
   }
 }
